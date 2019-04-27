7a2a1635dc8f71e7978f60cd0c8f2600c27643ac
Lance Ahn <lance@protopie.io>
2019-04-27 22:09:32 +0900

    chore: change 404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,7 +13,7 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <Head title="404: Not Found" />
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>You just hit a route that doesn&#39;t exist... 😢</p>
       </Layout>
     )
   }
