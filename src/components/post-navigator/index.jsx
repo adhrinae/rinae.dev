9f639e692c5e2ec373cb8572c4612dac61ad4fde
Lance Ahn <lance@protopie.io>
2019-04-28 14:33:27 +0900

    fix: set previous & next posts patth

--- src/components/post-navigator/index.jsx
+++ src/components/post-navigator/index.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link } from 'gatsby'
+import { divideDateAndPath } from '../../utils/frontmatter'
 
 import './index.scss'
 
 export const PostNavigator = ({ pageContext }) => {
   const { previous, next } = pageContext
+  const [, previousPath] = useMemo(
+    () => (previous ? divideDateAndPath(previous.fields.slug) : []),
+    [previous]
+  )
+  const [, nextPath] = useMemo(
+    () => (next ? divideDateAndPath(next.fields.slug) : []),
+    [next]
+  )
 
   return (
     <ul className="navigator">
       <li>
         {previous && (
-          <Link to={previous.fields.slug} rel="prev">
+          <Link to={previousPath} rel="prev">
             ← {previous.frontmatter.title}
           </Link>
         )}
       </li>
       <li>
         {next && (
-          <Link to={next.fields.slug} rel="next">
+          <Link to={nextPath} rel="next">
             {next.frontmatter.title} →
           </Link>
         )}
