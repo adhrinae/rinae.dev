1403019457cf9040dc0833149f1bd092845a52f0
Lance Ahn <lance@protopie.io>
2019-04-28 17:05:10 +0900

    fix: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,9 @@ module.exports = {
     graphql: false,
   },
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      experimentalObjectRestSpread: true,
       jsx: true,
     },
   },
