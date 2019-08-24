f994135902ff832286dd8a596901212ade66e743
Dohyung Ahn <me@rinae.dev>
2019-08-24 15:14:58 +0900

    fix: use parseISO for date string
    * following date-fns v2.0 update

--- src/utils/frontmatter.js
+++ src/utils/frontmatter.js
@@ -1,11 +1,11 @@
-import format from 'date-fns/format'
+import { format, parseISO } from 'date-fns'
 
 const SLUG_CAPTURE_REGEX = /^\/(\d{4}-\d{2}-\d{2})-(.+)\//
 
 export function divideDateAndPath(slug) {
   const capturedByGroups = SLUG_CAPTURE_REGEX.exec(slug)
   return [
-    format(capturedByGroups[1], 'YYYY/MM/DD'),
+    format(parseISO(capturedByGroups[1]), 'yyyy/MM/dd'),
     `/posts/${capturedByGroups[2]}`,
   ]
 }
