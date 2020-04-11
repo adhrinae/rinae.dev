1860077713fd9f0f0d84517fd93d20b5c49d9eb6
Dohyung Ahn <me@rinae.dev>
2020-04-11 20:33:02 +0900

    fix: remove `gatsby-plugin-offline`
    I don't need to support the offline environment
    but It looks like the service worker created from this plugin
    somehow blocks connecting my blog in certain browsers(ex: iOS Safari)

--- gatsby-config.js
+++ gatsby-config.js
@@ -145,7 +145,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-lodash`,
