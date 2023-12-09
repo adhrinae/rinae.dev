08cb48f9f00b66ce89281d277e7f5f3f2f5b4595
Dohyung Ahn <me@rinae.dev>
2023-12-09 17:08:42 +0900

    refactor: remove time display

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -36,17 +36,5 @@ const FormattedDatetime = ({ datetime }: { datetime: string | Date }) => {
     day: "numeric",
   });
 
-  const time = myDatetime.toLocaleTimeString(LOCALE, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
-  return (
-    <>
-      {date}
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      {time}
-    </>
-  );
+  return date;
 };
